1c75d3753f2743f4bd5c3547a56f0053e6a3414f
korid24 <korid24.dev@gmail.com>
2020-08-26 14:04:02 +0700

    block api urls

--- shop_asistant_dj/shop_asistant_dj/apps/api/urls.py
+++ shop_asistant_dj/shop_asistant_dj/apps/api/urls.py
@@ -6,8 +6,8 @@ app_name = 'api'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('pls/', views.PurchasesListsView.as_view()),
-    path('purs/', views.PurchasesView.as_view()),
-    path('user/', views.UserDetailView.as_view()),
+    # path('pls/', views.PurchasesListsView.as_view()),
+    # path('purs/', views.PurchasesView.as_view()),
+    # path('user/', views.UserDetailView.as_view()),
     path('telegram_session/', views.TelegramSessionView.as_view()),
 ]
