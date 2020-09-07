df6acc1e4799b08f96934b51252651e0f825b1e7
korid24 <korid24.dev@gmail.com>
2020-09-07 21:58:52 +0700

    common

--- shop_asistant_dj/shop_asistant_dj/apps/api/urls.py
+++ shop_asistant_dj/shop_asistant_dj/apps/api/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('', views.index, name='index'),
     # path('pls/', views.PurchasesListsView.as_view()),
     # path('purs/', views.PurchasesView.as_view()),
     # path('user/', views.UserDetailView.as_view()),
